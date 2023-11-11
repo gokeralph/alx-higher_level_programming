893a8278548780f98ba29416c5020a37d1d48c2b
Raphael Adegoke <138774999+gokeralph@users.noreply.github.com>
2023-11-11 02:36:04 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if(a_dictionary):
-        return(dict((x, y*2) for x, y in a_dictionary.items()))
+    """muliply the value in a dictionary
+
+    Args:
+        a_dictionary: a dictionary
+
+    Returns:
+       the new dictionary with modified value
+    """
+    new_dict = {}
+    for i in a_dictionary:
+        new_dict[i] = a_dictionary[i] * 2
+    return (new_dict)
