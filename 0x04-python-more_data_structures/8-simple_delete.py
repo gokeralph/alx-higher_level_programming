d7ac7d22dd92d050cabf8eb276c38106eba85f0c
Raphael Adegoke <138774999+gokeralph@users.noreply.github.com>
2023-11-11 02:35:14 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if(a_dictionary):
-        if(a_dictionary.get(key)):
-            del a_dictionary[key]
-    return(a_dictionary)
+    """simple delete an element from dictionary
+
+    Args:
+        a_dictionary: the dictionary
+        key: the key
+
+    Returns:
+       return the new dictionary
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return (a_dictionary)
