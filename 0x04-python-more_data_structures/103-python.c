16f8e5ad6d7a7abb43f2ee78580070676ab6b7e9
Raphael Adegoke <138774999+gokeralph@users.noreply.github.com>
2023-11-11 03:04:37 +0100

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -6,29 +6,32 @@
  * Return: python bytes
  */
 
-void print_python_list(PyObject *p) {
+void print_python_list(PyObject *p) 
+{
     Py_ssize_t i, size;
     PyObject *item;
-
     printf("[*] Python list info\n");
     printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
     printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-    for (i = 0, size = PyList_Size(p); i < size; i++) {
+    for (i = 0, size = PyList_Size(p); i < size; i++) 
+    {
         item = PyList_GetItem(p, i);
         printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
     }
 }
 
-void print_python_bytes(PyObject *p) {
+    void print_python_bytes(PyObject *p) 
+    {
     Py_ssize_t i, size;
     char *str;
 
-    printf("[.] bytes object info\n");
-    if (!PyBytes_Check(p)) {
+        printf("[.] bytes object info\n");
+        if (!PyBytes_Check(p)) 
+        {
         printf("  [ERROR] Invalid Bytes Object\n");
         return;
-    }
+        }
 
     size = PyBytes_Size(p);
     printf("  size: %ld\n", size);
@@ -36,7 +39,8 @@ void print_python_bytes(PyObject *p) {
 
     printf("  first 10 bytes: ");
     str = PyBytes_AsString(p);
-    for (i = 0; i < size && i < 10; i++) {
+    for (i = 0; i < size && i < 10; i++) 
+    {
         printf("%02hhx", str[i]);
         if (i < 9 && i < size - 1)
             printf(" ");
