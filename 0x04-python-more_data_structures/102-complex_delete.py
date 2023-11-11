d58d9f090c5a648f385066b3bdfae2cb0f41a5f4
Raphael Adegoke <138774999+gokeralph@users.noreply.github.com>
2023-11-11 02:27:32 +0100

    Update to 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if(value):
-        for key, val in list(a_dictionary.items()):
-            if val is value:
-                del a_dictionary[key]
-        return(a_dictionary)
+    """complex delete in a dictionary
+
+    Args:
+        a_dictionary: the dictionary
+        value in the dictionary
+
+    Returns:
+        the dictionary with change value
+    """
+    new_list = []
+    for i, j in a_dictionary.items():
+        if j == value:
+            new_list.append(i)
+    for m in new_list:
+        del a_dictionary[m]
+    return (a_dictionary)
