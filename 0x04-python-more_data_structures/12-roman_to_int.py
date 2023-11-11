1b53bb6a0c2a8a655d30fa92df18e1d452b7fa5a
Raphael Adegoke <138774999+gokeralph@users.noreply.github.com>
2023-11-11 02:29:50 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    sum = 0
-    if(roman_string and isinstance(roman_string, str)):
-        iT = {'D': 500, 'C': 100, 'M': 1000, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-        aList = [iT.get(x) for x in roman_string]
-        for idx, char in enumerate(aList):
-            if(idx < len(aList) - 1):
-                if aList[idx + 1] > char:
-                    sum -= char
-                else:
-                    sum += char
-            else:
-                sum += char
-    return(sum)
+    """Roman to Int
+
+    Args:
+        roman_string: the roman string
+
+    Returns:
+        the arabic numeral
+    """
+    if type(roman_string) != str or roman_string is None:
+        return (0)
+    roman = ['M', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX',
+             'V', 'IV', 'I']
+    arabic = [1000, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    length = len(roman_string)
+    number = 0
+    a = 0
+    while length != 0:
+        if length == 1:
+            idx = roman.index(roman_string[a])
+            number += arabic[idx]
+            return (number)
+        try:
+            idx = roman.index(roman_string[a: a + 2])
+            number += arabic[idx]
+            length -= 2
+            a += 2
+        except ValueError:
+            idx = roman.index(roman_string[a])
+            number += arabic[idx]
+            length -= 1
+            a += 1
+    return (number)
